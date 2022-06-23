6e89ea6c9458b99ec8cd70f5927fb9ffd50d4b76
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-06-22 21:06:02 -0500

    done with v hard

--- hard.js
+++ hard.js
@@ -1,53 +1,21 @@
-employees = {
-    John:{
-        id: '0001',
-        salary: 23000,
-        department: 'sanitation'
-    }, 
-    Thierno:{
-        id: '0002',
-        salary: 98000,
-        department: 'Back_end'
-    }, 
-    Ananyeli:{
-        id: '0003',
-        salary: 101000,
-        department: 'Back_end'
-    },
-    Chris:{
-        id: '0004',
-        salary: 87000,
-        department: 'UI Designer'
-    },
-    Tyali:{
-        id: '0005',
-        salary: 110000,
-        department: 'Product Manager'
-    },
-    Tyra:{
-        id: '0006',
-        salary: 102000,
-        department: 'UI Designer'
-    },
-    Dante:{
-        id: '0007',
-        salary: 78000,
-        department: 'UX Design'
-    },
-    Tyson:{
-        id: '0008',
-        salary: 231000,
-        department: 'Executive'
-    },
-    Matthew:{
-        id: '0009',
-        salary: 143000,
-        department: 'Back_end'
-    },
-    Luke:{
-        id: '0010',
-        salary: 23000,
-        department: 'Sanitation'
-    },
-
-}
+const express = require("express");
+const app = express();
+// const morgan = require("morgan");
+const fs = require("fs");
+const port = 8080;
+// app.use(express.json());
+const data = require("./employee.json");
+app.get("/", function (req, res) {
+  res.json(data);
+});
+app.get("/:id", function (req, res) {
+  const id = req.params.id;
+  res.json(data[id]);
+});
+app.listen(port, () => {
+  console.log(`it's live on http://localhost:${port}`);
+});
+app.use((req, res) => {
+  res.status(404).send("Sorry , wrong ID!");
+  //   res.end();
+});
